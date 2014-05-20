a44dfa2823891b734c7b98f5b2ff2b0220cd7a00
Sergio Tapia <sergiotapia@outlook.com>
2014-05-20 18:55:59 -0400

    Update readme and upload finished code.

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-Instructions and general purpose info comming soon!
+Sprinkle some paprika over your models and instantly have it 
+served via REST API.
+
+## Getting Started
+
+Paprika is really simple to use. All you need to do is attach
+your structs to it and it'll handle the rest.
+
+--- go
+package main
+
+import (
+    "fmt"
+    "log"
+
+    "github.com/sergiotapia/paprika"
+)
+
+type Product struct {
+    Name     string
+    Quantity int
+}
+
+func main() {
+    paprika.Attach(new(Product))
+    paprika.Start("8080")
+    log.Print("Paprika is up and running.")
+}
+---
+
+## How does it work?
+
+Paprika uses reflection to determine what the attached resource Type
+and attaches the appropriate REST URL's to the http server.
+
+## Work in Progress
+
+Paprika is still not complete! I've bitten off a bit more than I can chew
+but hopefully I can circle back soon and complete it. I think it'll be 
+really helpful for getting an API up and running quickly using Go.
+
+Particularly I'm having trouble figuring out how to store many different
+structs since Paprika cannot access structs defined in your calling go
+program.
+
+Ideally you will be able to set up your API in 5 minutes flat.
--- paprika.go
+++ paprika.go
@@ -2,8 +2,58 @@ package paprika
 
 import (
 	"fmt"
+	"net/http"
+	"os"
+	"reflect"
+	"strings"
+
+	"github.com/bmizerany/pat"
 )
 
-func main() {
+var mux = pat.New()
 
+func Attach(route string, resource interface{}) {
+	// Grab the resource Type name to use in our Mux.
+	resourceName := string(reflect.TypeOf(resource).String())
+	resourceName = resourceName[strings.Index(resourceName, ".")+1:]
+
+	// This just kills the server.
+	mux.Get("/kill", http.HandlerFunc(kill))
+
+	// Apply typical REST actions to Mux.
+	// ie: Product - to /products
+	mux.Get("/"+resourceName+"s", http.HandlerFunc(index))
+
+	// ie: Product ID: 1 - to /products/1
+	mux.Get("/"+resourceName+"/:id", http.HandlerFunc(show))
+}
+
+func Start(port string) {
+	// Hand off our configured Mux to the http server.
+	http.Handle("/", mux)
+	http.ListenAndServe(":"+port, nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	resourceType := r.URL.String()[1:]
+	x := Product{"Abc", 20}
+	w.Write([]byte(fmt.Sprintf("%v", params)))
+}
+
+func show(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query()
+	id := params.Get(":id")
+	w.Write([]byte("Hello " + id))
+}
+
+func kill(w http.ResponseWriter, r *http.Request) {
+	os.Exit(0)
+}
+
+type ResourceManager interface {
+	add() error
+	delete() error
+	update(id int) error
+	show(id int) error
+	list() error
 }
