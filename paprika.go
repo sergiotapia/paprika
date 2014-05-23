6f18abab6ab530b24c6b31779c102dd2f0f1f7bb
Sergio Tapia <sergiotapia@outlook.com>
2014-05-22 21:17:28 -0400

    Paprika now loads database configuration from database.toml file.

--- paprika.go
+++ paprika.go
@@ -17,37 +17,23 @@ func Attach(route string, resource interface{}) {
 	resourceName := string(reflect.TypeOf(resource).String())
 	resourceName = resourceName[strings.Index(resourceName, ".")+1:]
 
-	// This just kills the server.
 	mux.Get("/kill", http.HandlerFunc(kill))
-
-	// Apply typical REST actions to Mux.
-	// ie: Product - to /products
-	mux.Get("/"+resourceName+"s", http.HandlerFunc(index))
-
-	// ie: Product ID: 1 - to /products/1
-	mux.Get("/"+resourceName+"/:id", http.HandlerFunc(show))
+	mux.Get("/settings", http.HandlerFunc(settings))
 }
 
 func Start(port string) {
+	loadDatabaseConfiguration()
 	// Hand off our configured Mux to the http server.
 	http.Handle("/", mux)
 	http.ListenAndServe(":"+port, nil)
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
-	resourceType := r.URL.String()[1:]
-	x := Product{"Abc", 20}
-	w.Write([]byte(fmt.Sprintf("%v", params)))
-}
-
-func show(w http.ResponseWriter, r *http.Request) {
-	params := r.URL.Query()
-	id := params.Get(":id")
-	w.Write([]byte("Hello " + id))
-}
-
 func kill(w http.ResponseWriter, r *http.Request) {
 	os.Exit(0)
+}
+
+func settings(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(fmt.Sprintf("%v", database_setting)))
 }
 
 type ResourceManager interface {
